1318f2c3e625a3d0cdeb18098ccc708e133ccd0c
Jessica Son <jessicason@airbears2-10-142-124-214.airbears2.1918.berkeley.edu>
2018-04-01 20:55:20 -0700

    started on click functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,16 @@
 var emotion = ["excited", "hungry", "awkward", "bored", "confused"];
 var main = $("body");
 var btns = main.find("#buttons");
+var emotionBtn; var newEmotion; var clickedBtn;
 
 renderButtons();
+addEmotion();
 
+//this function creates the buttons
 function renderButtons() {
     for (var i = 0; i < emotion.length; i++) {
-        var emotionBtn = $("<button>");
-        emotionBtn.addClass("emotionBtn emotion emotionBtncolor");
+        emotionBtn = $("<button>");
+        emotionBtn.addClass("emotionBtn");
         emotionBtn.attr("data-emotion", emotion[i]);
         emotionBtn.text(emotion[i]);
         btns.append(emotionBtn);       
@@ -18,12 +21,24 @@ function renderButtons() {
                 console.log("Btns: ", btns);
 }
 
-$("#add-emotion").on("click", function(event) {
-    var newEmotion = $("#emotion-input").val().trim();
-    emotion.push(newEmotion);
-    $("#buttons").empty();
-    renderButtons();
+//this function adds new buttons
+function addEmotion(){ 
+    $("#add-emotion").on("click", function(event) {
+        newEmotion = $("#emotion-input").val().trim();
+        emotion.push(newEmotion);
+        $("#buttons").empty();
+        renderButtons();
+    });
+}
+
+
+$(btns).on("click", function(){
+    console.log("BUTTON WAS CLICKED!!!");
+    clickedBtn = $(this).;
+    console.log("clickedBtn: ", clickedBtn);
+
 });
+
 
 
 
