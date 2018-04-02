3edb2c4725bb17e52e5aba16dc66bb47d7dbaf30
Jessica Son <jessicason@airbears2-10-142-124-214.airbears2.1918.berkeley.edu>
2018-04-01 18:39:36 -0700

    appending buttons work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,16 +2,30 @@ var place = ["Alaska", "Barbados", "Cancun", "Denver", "Fullerton", "Georgia", "
 var main = $("body");
 var btns = main.find("#buttons");
 
-for (var i = 0; i < place.length; i++) {
-    var placeBtn = $("<button>");
-    placeBtn.addClass("placebtn place placebtncolor");
-    placeBtn.attr("data-place", place[i]);
-    placeBtn.text(place[i]);
-    btns.append(placeBtn);       
+renderButtons();
+
+function renderButtons() {
+    for (var i = 0; i < place.length; i++) {
+        var placeBtn = $("<button>");
+        placeBtn.addClass("placebtn place placebtncolor");
+        placeBtn.attr("data-place", place[i]);
+        placeBtn.text(place[i]);
+        btns.append(placeBtn);       
+    }
+                console.log("this works");
+                console.log("Main: ", main);
+                console.log("placeBtn: ", placeBtn);
+                console.log("Btns: ", btns);
 }
-            console.log("this works");
-            console.log("Main: ", main);
-            console.log("placeBtn: ", placeBtn);
-            console.log("Btns: ", btns);
+
+$("#add-place").on("click", function(event) {
+    var newPlace = $("#place-input").val().trim();
+    place.push(newPlace);
+    $("#buttons").empty();
+    renderButtons();
+});
+
+
+
 
 
