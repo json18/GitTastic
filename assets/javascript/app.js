c5b5b80cc25b20c41bf92d9f940534aa63d6da0d
Jessica Son <jessicason@airbears2-10-142-124-214.airbears2.1918.berkeley.edu>
2018-04-01 18:52:56 -0700

    changed category to emotions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,26 +1,26 @@
-var place = ["Alaska", "Barbados", "Cancun", "Denver", "Fullerton", "Georgia", "Hawaii", "Iowa", "Japantown", "Koreatown", "Los Angeles", "Manhattan", "Nebraska", "Omaha", "Philadelphia", "Queens", "Riverside", "Seattle", "Washington D.C."];
+var emotion = ["excited", "hungry", "awkward", "bored", "confused"];
 var main = $("body");
 var btns = main.find("#buttons");
 
 renderButtons();
 
 function renderButtons() {
-    for (var i = 0; i < place.length; i++) {
-        var placeBtn = $("<button>");
-        placeBtn.addClass("placebtn place placebtncolor");
-        placeBtn.attr("data-place", place[i]);
-        placeBtn.text(place[i]);
-        btns.append(placeBtn);       
+    for (var i = 0; i < emotion.length; i++) {
+        var emotionBtn = $("<button>");
+        emotionBtn.addClass("emotionBtn emotion emotionBtncolor");
+        emotionBtn.attr("data-emotion", emotion[i]);
+        emotionBtn.text(emotion[i]);
+        btns.append(emotionBtn);       
     }
                 console.log("this works");
                 console.log("Main: ", main);
-                console.log("placeBtn: ", placeBtn);
+                console.log("emotionBtn: ", emotionBtn);
                 console.log("Btns: ", btns);
 }
 
-$("#add-place").on("click", function(event) {
-    var newPlace = $("#place-input").val().trim();
-    place.push(newPlace);
+$("#add-emotion").on("click", function(event) {
+    var newEmotion = $("#emotion-input").val().trim();
+    emotion.push(newEmotion);
     $("#buttons").empty();
     renderButtons();
 });
