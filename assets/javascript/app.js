598e47f61a8fc6411889605bbd3732001b2e5ce3
Jessica Son <jessicason@airbears2-10-142-124-214.airbears2.1918.berkeley.edu>
2018-04-01 18:11:16 -0700

    added buttons

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+var place = ["Alaska", "Barbados", "Cancun", "Denver", "Fullerton", "Georgia", "Hawaii", "Iowa", "Japantown", "Koreatown", "Los Angeles", "Manhattan", "Nebraska", "Omaha", "Philadelphia", "Queens", "Riverside", "Seattle", "Washington D.C."];
+var main = $("body");
+var btns = main.find("#buttons");
+
+for (var i = 0; i < place.length; i++) {
+    var placeBtn = $("<button>");
+    placeBtn.addClass("placebtn place placebtncolor");
+    placeBtn.attr("data-place", place[i]);
+    placeBtn.text(place[i]);
+    btns.append(placeBtn);       
+}
+            console.log("this works");
+            console.log("Main: ", main);
+            console.log("placeBtn: ", placeBtn);
+            console.log("Btns: ", btns);
+
+
